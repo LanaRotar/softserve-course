209452c4180abd22eec04cf7e0186c55bca26d9f
LanaRotar <roxolana.oslavska@gmail.com>
2020-02-26 22:49:39 +0200

    added tasks 1.2, 3.1, 4.1

---
+++ homework-1/task-1.2-3.1-4.1.js
@@ -0,0 +1,57 @@
+function calculation (x) {   //Conditional designs 1
+	if (x > 0) {
+		return x+1;
+	}
+	if (x < 0) {
+		return x-2;
+	}
+	if (x == 0){
+		return 10;
+	}
+}
+
+console.log(calculation(7));
+
+
+
+
+var y;   // Variables tack 3
+y = 5;
+console.log(y);
+y = 6;
+console.log(y);
+
+
+
+
+var y; var x;   // Variables task 4
+y = 3; x = 4;
+console.log(y, x);
+y = x;
+console.log(y, x);
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
